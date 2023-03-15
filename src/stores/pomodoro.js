3d7305b99e810fdfaf637a5d430fd96899b5ac8c
Finestwork <Younes.Espiritu@outlook.com>
2023-03-16 03:56:50 +0800

    Add placeholder

--- src/stores/pomodoro.js
+++ src/stores/pomodoro.js
@@ -19,15 +19,28 @@ const useStoredPomodoros = defineStore('storedPomodoros', {
   actions: {
     storeCloudPomodoros(pomodoros) {
       // Sort by oldest to latest
-      this.pomodoros = pomodoros
-        .map((doc) => {
-          const DATA = doc.data();
-          return {
-            pomodoros: DATA.pomodoros,
-            date: DATA.date
-          };
-        })
-        .sort((a, b) => new Date(a.date) - new Date(b.date));
+      this.pomodoros = pomodoros.map((doc) => {
+        const DATA = doc.data();
+        return {
+          pomodoros: DATA.pomodoros,
+          date: DATA.date
+        };
+      });
+
+      // If recent activity is only 1 then add a padding before the current date
+      if (this.pomodoros.length === 1) {
+        let date = new Date(this.pomodoros[0].date);
+        date = new Date(date.setDate(date.getDate() - 1));
+        this.pomodoros.push({
+          pomodoros: -1,
+          date: date.toLocaleDateString('en', {
+            dateStyle: 'short'
+          })
+        });
+      }
+
+      // Then sort from oldest to latest date
+      this.pomodoros.sort((a, b) => new Date(a.date) - new Date(b.date));
     },
 
     // Only increment by 1 the today pomodoro activity
